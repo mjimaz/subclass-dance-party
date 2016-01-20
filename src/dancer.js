d80dc8293de509a98c16deca1c6477fccd49b6cd
mjimaz <m.aljimaz@gmail.com>
2016-01-19 20:02:05 -0800

    working on dragging

--- src/dancer.js
+++ src/dancer.js
@@ -1,22 +1,47 @@
 var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer makeMeDraggable" ></span>');
 // this.$node.mousedown(
-//   function(){
-//   // console.log('drag:', this);
-//   //     var top = event.clientY;
-//   //     var left = event.clientX;
-//   //   // console.log('inside drag this=', this);
-//   //   console.log('top=', top, "left=", left);
-//     //   this.styleSettings = {
-//     //    top: top,
-//     //    left: left
-//     // };
-//     // $(this).attr('style', "top: "+top+"px; left: "+left+"px;");
-//     $(this).draggable();
+//   function(event){
+//   console.log('drag:', this);
+//       var top = event.pageY;
+//       var left = event.pageX;
+//     // console.log('inside drag this=', this);
+//     console.log('top=', top, "left=", left);
+//       this.styleSettings = {
+//        top: top,
+//        left: left
+//     };
+//     $(this).attr('style', "top: "+top+"px; left: "+left+"px;");
+// //     $(this).draggable();
 
 
 //     }
 //   );
+var isMouseDown = false;
+
+var time;
+
+this.$node.mousedown(function(e){
+  var context = this;
+  console.log('mouse down, context:', context);
+  time = setInterval(function(e, context){
+    return function(){dragDancer(e, context);};
+  }, 1000);
+}
+  );
+
+var dragDancer = function(event, context){
+  console.log('dragmouse up: context, ', context);
+  var e = window.event;
+  $(context).css({left:e.pageX - 40, top:e.pageY - 40});
+};
+
+this.$node.mouseup(function(e){
+  isMouseDown = false;
+  console.log('mouse up isMouseDown', isMouseDown);
+    //$(this).css({left:e.pageX - 40, top:e.pageY - 40});
+    clearInterval(time);
+});
 
 // $(this).draggable({
 //   drag: function( event, ui ) {
